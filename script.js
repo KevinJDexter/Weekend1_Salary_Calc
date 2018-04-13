d7f868cd9b141a30a08f35c922747d46c20d6f86
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-13 16:12:54 -0500

    Initial HTML and JS getters

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(onReady);
+
+function onReady() {
+  let totalMonthlySalary = $('#cumulativeMonthlySalary');
+}
+
+function addEmployee () {
+  
+}
+
+function getFirstName() {
+  return $('#fNameInput').val()
+}
+
+function getLastName() {
+  return $('#lNameInput').val()
+}
+
+function getId() {
+  return $('#idInput').val()
+}
+
+function getTitle() {
+  return $('#titleInput').val()
+}
+
+function getSalary() {
+  return $('#salaryInput').val()
+}
